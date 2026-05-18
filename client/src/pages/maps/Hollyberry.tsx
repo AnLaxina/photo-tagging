b6d6d777d62ecf168ebdf98742f100e1f7ad6e7e
AnLaxina <anilovlaxina@gmail.com>
2026-05-18 15:11:22 -0700

    feat(hollyberry): add layout & movable map

--- client/src/pages/maps/Hollyberry.tsx
+++ client/src/pages/maps/Hollyberry.tsx
@@ -1,20 +1,23 @@
 import type { LatLngBoundsExpression } from "leaflet";
 import L from "leaflet";
-import { MapContainer, ImageOverlay } from "react-leaflet";
+import { MapContainer, ImageOverlay, Rectangle } from "react-leaflet";
 
 export default function HollyberryMap() {
   const imageBounds: LatLngBoundsExpression = [
     [0, 0],
     [616, 1024],
   ];
+
   return (
     <MapContainer
       crs={L.CRS.Simple}
       maxBounds={imageBounds}
       center={[616 / 2, 1024 / 2]}
-      zoom={0}
-      maxZoom={0}
+      zoom={0.5}
+      maxZoom={1}
+      minZoom={1}
       className="min-h-dvh"
+      zoomControl={false}
       attributionControl={false}
     >
       <ImageOverlay
