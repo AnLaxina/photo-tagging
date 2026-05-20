8459c23a176781365ed7ec846e18d178570ed4db
AnLaxina <anilovlaxina@gmail.com>
2026-05-19 23:38:22 -0700

    feat(popup): add instructions

--- client/src/components/Popup.tsx
+++ client/src/components/Popup.tsx
@@ -8,13 +8,28 @@ interface PopupProps {
 export default function Popup({ parentRef }: PopupProps) {
   return (
     <motion.dialog
-      className="open:backdrop:bg-[rgba(0,0,0,0.7)] open:self-center open:justify-self-center"
+      className="open:flex flex-col gap-2 open:backdrop:bg-[rgba(0,0,0,0.7)] open:self-center open:justify-self-center p-8 rounded-md border-4 border-solid"
       ref={parentRef}
       initial={{ scale: 0 }}
       animate={{ scale: 1 }}
       exit={{ opacity: 0 }}
     >
-      <p>Cool Dialog!</p>
+      <h1 className="text-2xl font-semibold text-center">Instructions</h1>
+      <ol className="flex flex-col gap-4">
+        <li>
+          <p>1. Find the following cookies:</p>
+          <p>Cookie placeholder (should be a prop that displays images)</p>
+        </li>
+        <li>
+          <p>
+            2. Click/tap on a cookie once found, and select the correct one via
+            popup.
+          </p>
+        </li>
+        <li>
+          <p>3. Once done, input your name to enter the leaderboards!</p>
+        </li>
+      </ol>
     </motion.dialog>
   );
 }
